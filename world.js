b70074d30af43c6fb6ed491fd23729fafed26712
Joooo15 <jobrandon2004@gmail.com>
2025-11-27 10:32:43 -0600

    Happy Thanksgiving!!!

---
+++ world.js
@@ -0,0 +1,23 @@
+var menu = document.getElementById("hamburger-menu")
+var hamburger = document.getElementById("hamburger-icon")
+var x = document.getElementById("x")
+
+function showMenu() {
+    menu.style.display = "flex";
+    requestAnimationFrame(() => {
+        menu.style.transform = "translate(0%)";
+    })  
+}
+
+function exit() {
+    menu.style.transform = "translate(100%)"
+    menu.addEventListener('transitionend', hideMenu)
+}
+
+function hideMenu() {
+    menu.style.display = "none";
+    menu.removeEventListener('transitionend', hideMenu);
+}
+
+hamburger.addEventListener("click", showMenu)
+x.addEventListener("click", exit)
